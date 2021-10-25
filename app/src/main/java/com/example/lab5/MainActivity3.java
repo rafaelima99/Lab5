f204e1946bbe6095c037ff2f668072055242d7fa
rafaelima99 <43426335+rafaelima99@users.noreply.github.com>
2021-10-25 15:28:06 -0500

    Add to database implemented

--- app/src/main/java/com/example/lab5/MainActivity3.java
+++ app/src/main/java/com/example/lab5/MainActivity3.java
@@ -2,10 +2,17 @@ package com.example.lab5;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity3 extends AppCompatActivity {
 
@@ -18,9 +25,39 @@ public class MainActivity3 extends AppCompatActivity {
 
         EditText editText = findViewById(R.id.editText);
         Intent intent = getIntent();
-        //noteid = intent.getIntExtra();
+        noteid = intent.getIntExtra("noteid", -1);
+
+        if (noteid != -1) {
+            Note note = MainActivity2.notes.get(noteid);
+            String noteContent = note.getContent();
+            editText.setText(noteContent);
+        }
     }
 
-    public void buttonClick(View view) {
+    public void saveMethod(View view) {
+        EditText editText = findViewById(R.id.editText);
+        String content = editText.getText().toString();
+
+        Context context = getApplicationContext();
+        SQLiteDatabase sqLiteDatabase = context.openOrCreateDatabase("notes", Context.MODE_PRIVATE,null);
+        DBHelper database = new DBHelper(sqLiteDatabase);
+        SharedPreferences sharedPreferences = getSharedPreferences("com.example.lab5", Context.MODE_PRIVATE);
+
+        String username = sharedPreferences.getString("username", "");
+
+        String title;
+        DateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
+        String date = dateFormat.format(new Date());
+
+        if (noteid == -1) {
+            title = "NOTE_" + (MainActivity2.notes.size() + 1);
+            database.saveNotes(username, title, content, date);
+        } else {
+            title = "NOTE_" + (noteid + 1);
+            database.updateNote(title, date, content, username);
+        }
+
+        Intent intent = new Intent(this, MainActivity2.class);
+        startActivity(intent);
     }
 }
